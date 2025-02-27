239ae333645d1e1528764d0609cd186b40d8b139
MaryamM4 <maryamma@outlook.com>
2025-02-27 00:36:53 +0000

    Fixed schedule_priority not comparing to head and incorrectly using Quantum (should just use burst).

--- src/schedule_priority.c
+++ src/schedule_priority.c
@@ -5,6 +5,8 @@
 
 struct node *g_head = NULL;
 
+int min(int a, int b) { return (a < b) ? a : b; }
+
 void add(char *name, int priority, int burst) {
   Task *newTask = createTask(name, priority, burst);
 
@@ -12,7 +14,8 @@ void add(char *name, int priority, int burst) {
   newNode->task = newTask;
   newNode->next = NULL;
 
-  if (g_head == NULL) {
+  if (g_head == NULL || priority > g_head->task->priority) {
+    newNode->next = g_head;
     g_head = newNode;
 
   } else {
@@ -35,14 +38,15 @@ void add(char *name, int priority, int burst) {
 
 void schedule() {
   Task *temp;
-  struct node *it = malloc(sizeof(struct node));
-  it = g_head;
+  struct node *it = g_head;
 
   while (g_head != NULL) {
-    run(it->task, min(it->task->burst, QUANTUM));
+    run(it->task, it->task->burst);
 
     temp = it->task;
     it = it->next;
+
+    deleteFromList(&g_head, temp);
     deleteTask(temp);
   }
 }
